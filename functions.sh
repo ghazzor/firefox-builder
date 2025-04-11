cc028bb5b6d4f3ab52cac872e61f0885ba7bd6c7
ghazzor <kartikhis8o@gmail.com>
2025-04-12 00:55:19 +0530

    build.sh: switch to wget from curl & refine script curl can give empty files so switch to wget
    move sync_ver and build_deb to functions.sh
    
    Signed-off-by: ghazzor <kartikhis8o@gmail.com>

---
+++ functions.sh
@@ -0,0 +1,77 @@
+sync_ver() {
+    echo "Fetching latest Firefox version..."
+    VERSION=$(curl -sf https://product-details.mozilla.org/1.0/firefox_versions.json | \
+             jq -er '.LATEST_FIREFOX_VERSION')
+    export VERSION
+    ver=$VERSION
+
+    TAG="FIREFOX_$( echo ${ver} | sed 's/\./_/g')_RELEASE"
+
+    echo "Syncing to $TAG"
+    hg up -C "$TAG" || {
+        echo "Error: Failed to update to version $TAG"
+        exit 1
+    }
+}
+
+build_deb() {
+export PACKAGE=firefox-lp3
+
+cp mozilla-unified/objdir-opt/dist/*.tar.* .
+tar xvf *.tar.*
+
+rm -rf $PACKAGE
+mkdir -p $PACKAGE/DEBIAN
+mkdir -p $PACKAGE/opt
+mkdir -p $PACKAGE/usr/share/applications/
+mkdir -p $PACKAGE/opt/$PACKAGE/
+
+cp -r firefox/* $PACKAGE/opt/$PACKAGE/
+mv $PACKAGE/opt/$PACKAGE/firefox $PACKAGE/opt/$PACKAGE/$PACKAGE
+mv $PACKAGE/opt/$PACKAGE/firefox-bin $PACKAGE/opt/$PACKAGE/$PACKAGE-bin
+
+cp $PACKAGE.desktop $PACKAGE/usr/share/applications/$PACKAGE.desktop
+
+cat > $PACKAGE/DEBIAN/control <<EOF
+Package: $PACKAGE
+Architecture: amd64
+Maintainer: @ghazzor
+Priority: optional
+Version: $VERSION
+Description: Unofficial Firefox build for x86-64-v3 CPU with O3+LTO+PGO.
+EOF
+
+cat > $PACKAGE/DEBIAN/postinst <<EOF
+#!/bin/sh
+set -e
+
+# Create symbolic link after installation
+ln -sf /opt/$PACKAGE/$PACKAGE /usr/bin/$PACKAGE
+
+exit 0
+EOF
+
+chmod 755 $PACKAGE/DEBIAN/postinst
+
+cat > $PACKAGE/DEBIAN/postrm <<EOF
+#!/bin/sh
+set -e
+
+case "\$1" in
+    remove|purge)
+        rm -f /usr/bin/$PACKAGE
+        ;;
+esac
+EOF
+
+chmod 755 $PACKAGE/DEBIAN/postrm
+
+chmod +x $PACKAGE/opt/$PACKAGE/$PACKAGE
+chmod +x $PACKAGE/opt/$PACKAGE/$PACKAGE-bin
+chmod +x $PACKAGE/usr/share/applications/*.desktop
+
+# Build the package and check for errors
+dpkg-deb --build $PACKAGE || { echo "Error: Failed to create Debian package"; exit 1; }
+mv ${PACKAGE}.deb ${PACKAGE}_v${VERSION}_amd64.deb
+rm -rf *.tar.* firefox $PACKAGE
+}
